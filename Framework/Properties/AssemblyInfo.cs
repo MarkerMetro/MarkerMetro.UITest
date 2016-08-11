5989e4f3240b0a16fd8f81bdbdbb24e8812801ee
Andrew Revell <andrew.r@markermetro.com>
2016-08-11 14:23:53 +1200

    Changed for nuget packaging

--- Framework/Properties/AssemblyInfo.cs
+++ Framework/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Framework")]
+[assembly: AssemblyTitle("MarkerMetro.UITest.Framework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
